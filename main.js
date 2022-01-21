5524e65cb1aca1c0427fae7e897832005907b8c1
EC2 Default User <ec2-user@ip-172-31-26-64.ec2.internal>
2022-01-21 00:39:12 +0000

    課題提出

--- main.js
+++ main.js
@@ -2,15 +2,23 @@ function push(enter) {
   let answer = document.getElementById("answer")
   let enter_namber = enter.innerHTML;
   
+  
+  
   if (enter_namber == "AC") {
     answer.innerHTML = "0"
-  } else if (enter_namber == "=") {
+  }else if(enter_namber == "=") {
     answer.innerHTML = eval(answer.innerHTML)
   } else{
     if (answer.innerHTML == "0") {
       answer.innerHTML = enter_namber
-    } else {
+    }else {
       answer.innerHTML += enter_namber
     }
   }
+  if(enter_namber.slice(-1) == "+" || "-" || "*" || "/"){
+   let enz = enter_number.slice(-1);
+   enz = enter_namber
+   answer.HTML = enz
+ }
 }
+ 
