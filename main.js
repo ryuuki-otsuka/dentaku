adb8d53db11ff531ae05e6c454de4decb89fc87a
EC2 Default User <ec2-user@ip-172-31-26-64.ec2.internal>
2022-01-22 08:04:36 +0000

    課題提出

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-function huu(enter){
+function push(enter){
   let answer = document.getElementById("answer")
   let enter_number = enter.innerHTML;
-  console.log("大丈夫");
+  
   if(enter_number == "AC"){
     answer.innerHTML = "0"
   }else if (enter_number == "="){
