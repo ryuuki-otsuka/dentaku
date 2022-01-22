c206ace848e8f83df9f66298a1799aaf8a7b9094
EC2 Default User <ec2-user@ip-172-31-26-64.ec2.internal>
2022-01-22 03:36:30 +0000

    課題提出

--- main.js
+++ main.js
@@ -1,31 +1,16 @@
-function push(enter) {
+function huu(enter){
   let answer = document.getElementById("answer")
   let enter_number = enter.innerHTML;
-  
-  
-  
-  if (enter_number == "AC") {
+  console.log("大丈夫");
+  if(enter_number == "AC"){
     answer.innerHTML = "0"
-  }else if(enter_number == "=") {
+  }else if (enter_number == "="){
     answer.innerHTML = eval(answer.innerHTML)
-  } else{
-    if (answer.innerHTML == "0") {
-      answer.innerHTML = enter_number
-      }else{
-       answer.innerHTML += enter_number
-      }
+  }else{
+    if(answer.innerHTML == "0"){
+      answer.innerHTML == enter_number
+    }else{
+      answer.innerHTML += enter_number
     }
   }
-  
-  
-  function huu (enter) {
-    let answer = document.getElementById("answer")
-    let enter_number = enter.innerHTML;
-    answer.innerHTML += enter_number
-    if(answer.innerHTML.slice(-1) == "+" || answer.innerHTML.slice(-1) == "-" || answer.innerHTML.slice(-1) == "*" || answer.innerHTML.slice(-1) == "/"){
-      console.log("大丈夫");
-        answer.innerHTML.slice(0, -1);
-   }else{
-     console.log("大丈夫でない");
-   }
-  }
+}
